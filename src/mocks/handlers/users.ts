41c3f01eb9d0bbff4af59b455274a572a4574379
kitsne <bicycle.y.t@gmail.com>
2025-06-14 00:47:49 +0900

    /api/me のデフォルト値を traP に

--- src/mocks/handlers/users.ts
+++ src/mocks/handlers/users.ts
@@ -1,10 +1,8 @@
 import { HttpResponse } from 'msw'
 import { http } from '@/mocks/http'
 
-const myID = import.meta.env.MY_TRAQ_ID || 'kitsne'
-
 export const usersHandlers = [
   http.get('/api/me', async () => {
-    return HttpResponse.json({ id: myID, isStaff: true })
+    return HttpResponse.json({ id: 'traP', isStaff: true })
   }),
 ]
