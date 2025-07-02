04336f2e48c48ef9d01a1dd90f34dc9303185908
kitsne241 <bicycle.y.t@gmail.com>
2025-07-02 17:20:52 +0900

    確実に最新の合宿を取得して camp の初期値とする

--- src/store.ts
+++ src/store.ts
@@ -10,12 +10,10 @@ export const useUserStore = defineStore('user', () => {
   const user = ref<User>()
 
   const initUser = async () => {
-    console.log('Hello')
     const { data, error } = await apiClient.GET('/api/me')
     if (error || !data) {
       throw Error(`ユーザー情報を取得できません: ${error}`)
     }
-    console.log('Success')
     user.value = data
     return user.value
   }
@@ -31,7 +29,10 @@ export const useCampStore = defineStore('camp', () => {
     if (camps.error || !camps.data) {
       throw Error(`合宿情報を取得できません: ${camps.error}`)
     }
-    const latestCamp = camps.data.filter((camp) => !camp.isDraft).pop()
+    const latestCamp = camps.data
+      .filter((camp) => !camp.isDraft)
+      .sort((a, b) => new Date(a.dateStart).getTime() - new Date(b.dateStart).getTime())
+      .pop()
     if (!latestCamp) {
       throw Error(`合宿が見つかりません`)
     }
