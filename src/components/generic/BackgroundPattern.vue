c3eda841b7323db7c203cc8cad7a349af49343ab
kitsne241 <bicycle.y.t@gmail.com>
2025-07-03 07:53:43 +0900

    BackgroundPattern への色の与え方を変更

--- src/components/generic/BackgroundPattern.vue
+++ src/components/generic/BackgroundPattern.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
-defineProps<{
-  logoColor: string;
-  bgColor: string;
-}>();
+import { computed, defineProps } from 'vue'
+
+const props = defineProps<{
+  variant: 'light' | 'primary'
+}>()
+
+const logoColor = computed(() => {
+  return props.variant === 'primary' ? '#FF8200' : 'var(--color-surface)'
+})
+
+const bgColor = computed(() => {
+  return props.variant === 'primary' ? 'var(--color-primary)' : 'var(--color-background)'
+})
 </script>
 
 <template>
-  <div :class="$style.background" :style="{backgroundColor: bgColor}">
+  <div :class="$style.background" :style="{ backgroundColor: bgColor }">
     <div :class="$style.center">
-      <div :class="$style.pattern" :style="{backgroundColor: logoColor}"></div>
+      <div :class="$style.pattern" :style="{ backgroundColor: logoColor }"></div>
     </div>
   </div>
-
-
 </template>
 
 <style module>
@@ -41,11 +47,13 @@ defineProps<{
   height: 8000px;
   width: 8000px;
   transform: rotate(-30deg);
-  mask-image:
-    url('/logo/logo-bg.svg'),
-    url('/logo/logo-bg.svg');
-  mask-size: 800px 600px, 800px 600px;
-  mask-position: 0 0, 400px 300px;
+  mask-image: url('/logo/logo-bg.svg'), url('/logo/logo-bg.svg');
+  mask-size:
+    800px 600px,
+    800px 600px;
+  mask-position:
+    0 0,
+    400px 300px;
   mask-repeat: repeat;
 }
 </style>
