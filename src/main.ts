fd618a13a042f5f340a07c4b58125efe8ce0df78
kitsne241 <bicycle.y.t@gmail.com>
2025-07-03 07:44:07 +0900

    main.ts の色設定の変更と簡素化に伴う置換

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
 import App from './App.vue'
 
-import './assets/main.css'
+import './assets/style.css'
 
 const app = createApp(App)
 const pinia = createPinia()
 
-// Vuetify
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import * as components from 'vuetify/components'
@@ -22,40 +21,16 @@ const vuetify = createVuetify({
     themes: {
       light: {
         colors: {
-          black: '#000000',
-          darkGray: '#444444',
-          gray: '#aaaaaa',
-          lightGray: '#e0e0e0',
-          white: '#ffffff',
-          exwhite: '#ffffff',
-
-          ash: '#444444', // darkGray に同じ。イベントの色名はこっちを採用
-          ashPale: '#e0e0e0', // lightGray に同じ
-
-          orange: '#ff7300',
-          orangePale: '#ffe2b2',
-          green: '#178000',
-          greenPale: '#acf69c',
-          red: '#ff4d00',
-          redPale: '#ffc9b2',
-          blue: '#008cff',
-          bluePale: '#9dd8ff',
-          navy: '#2444a4',
-          navyPale: '#afc3ff',
-          purple: '#7210C8',
-          purplePale: '#DEB9FF',
-          pink: '#EE4DBE',
-          pinkPale: '#FFB6E9',
-
-          theme: '#ff7300',
-          themePale: '#FFE5C9',
-
-          text: '#000000', // var(--color-black)
-          background: '#ffffff', // var(--color-background)
+          // Core colors
+          background: '#f6f6f6',
           surface: '#ffffff',
-          primary: '#ff7300', // var(--color-orange)
-          secondary: '#ffe2b2', // var(--color-orange-pale)
-          error: '#ff4d00', // var(--color-red)
+          primary: '#ff7300',
+          secondary: '#444444',
+          primaryLight: '#FFE5C9',
+
+          // Standard Vuetify colors
+          text: '#000000',
+          error: '#ff4d00',
         },
       },
     },
