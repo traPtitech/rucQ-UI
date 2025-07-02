4083439323c99d533ba3a7a5b02fb31a9204bd76
kitsne241 <bicycle.y.t@gmail.com>
2025-07-02 17:25:32 +0900

    表示用コンポーネントに model-value を渡す

--- src/components/information/QuestionEditField.vue
+++ src/components/information/QuestionEditField.vue
@@ -13,7 +13,7 @@ const requiredRule = (v: unknown) => (v === undefined || v === null ? '必須項
 <!-- prettier-ignore -->
 <template>
   <v-text-field
-    v-model="(value as string)"
+    :model-value="(value as string)"
     v-if="question.type === 'free_text'"
     :label="question.title"
     :messages="[question.description ?? '']"
@@ -22,7 +22,7 @@ const requiredRule = (v: unknown) => (v === undefined || v === null ? '必須項
     variant="underlined"
   ></v-text-field>
   <v-number-input
-    v-model="(value as number)"
+    :model-value="(value as number)"
     v-if="question.type === 'free_number'"
     :label="question.title"
     :messages="[question.description ?? '']"
@@ -32,7 +32,7 @@ const requiredRule = (v: unknown) => (v === undefined || v === null ? '必須項
     control-variant="hidden"
   ></v-number-input>
   <v-select
-    v-model="(value as number)"
+    :model-value="(value as number)"
     v-if="question.type === 'single'"
     :label="question.title"
     :messages="[question.description ?? '']"
@@ -44,7 +44,7 @@ const requiredRule = (v: unknown) => (v === undefined || v === null ? '必須項
     :item-title="(option) => option.content"
   ></v-select>
   <v-select
-    v-model="(value as number[])"
+    :model-value="(value as number[])"
     v-if="question.type === 'multiple'"
     :label="question.title"
     :messages="[question.description ?? '']"
