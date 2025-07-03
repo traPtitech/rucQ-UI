fed110fb4eb577aab454540718c9c76bb9bbc21f
kitsne241 <bicycle.y.t@gmail.com>
2025-07-03 09:52:41 +0900

    NotFoundView のデザイン変更

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,13 +1,6 @@
-<script setup lang="ts"></script>
-
 <template>
   <div :class="$style.container">
-    <router-link :to="`/`">
-      <img :src="`/logo/logo.svg`" style="width: 300px" />
-    </router-link>
-    <!-- vue-router によって同一サイト内部へのリンクを貼る場合、<a> タグの代わりに <router-link> を用いる -->
-
-    <h1 style="font-weight: 500">Not Found</h1>
+    <h1 :class="$style.title">Not Found</h1>
     <h2 :class="$style.path">{{ decodeURI($route.path) }}</h2>
   </div>
 </template>
@@ -19,19 +12,19 @@
   align-items: center;
   justify-content: center;
   height: 100%;
+  color: white;
+  position: relative;
+}
+
+.title {
+  font-size: 48px;
 }
 
 .path {
-  padding: 0 20px;
-  font-weight: bold;
-  background-color: var(--color-text-sub);
-  color: var(--color-background);
+  padding: 0px 20px;
+  font-size: 24px;
+  font-family: 'M PLUS Code Latin';
   border-radius: 6px;
-}
-
-.logo {
-  color: var(--color-text);
-  width: 50px;
-  /* border: 1px solid white; */
+  border: 1px solid white;
 }
 </style>
