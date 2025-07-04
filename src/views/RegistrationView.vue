be0112cd3fd295d231a99298464e3b72a9c076f6
kitsne241 <bicycle.y.t@gmail.com>
2025-07-04 16:50:32 +0900

    feat: 合宿のタイトルとしおりを適用

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,33 +1,73 @@
 <!-- 参加登録・合宿選択 View -->
 <!-- すでにユーザーが最新の合宿の参加登録を済ませている場合は GuidebookView へリダイレクトする -->
 <script setup lang="ts">
+import { useCampStore } from '@/store'
+import { storeToRefs } from 'pinia'
+// import MarkdownPreview from '@/components/markdown/MarkdownPreview.vue'
+const { displayCamp } = storeToRefs(useCampStore())
 </script>
 
-
-
 <template>
-  <div class="container">
-    <img :src="`/logo/logo-white.svg`" style="width: 300px" />
-    <v-expansion-panels class="panel">
-      <v-expansion-panel 
-        title="3290年度 Xmas合宿"
-      >
+  <div :class="$style.container">
+    <img :src="`/logo/logo-white.svg`" style="width: 200px" />
+    <v-expansion-panels v-if="displayCamp">
+      <v-expansion-panel :class="$style.panel">
+        <v-expansion-panel-title>
+          <span :class="$style.title">{{ displayCamp.name }}</span>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <div :class="$style.content">
+            <div>{{ displayCamp.description }}</div>
+            <v-btn
+              elevation="0"
+              prepend-icon="mdi-arrow-right"
+              baseColor="transparent"
+              variant="flat"
+              color="primary"
+              :class="[$style.save, 'font-weight-bold']"
+            >
+              <span class="font-weight-medium">この合宿に参加する</span>
+            </v-btn>
+          </div>
+        </v-expansion-panel-text>
       </v-expansion-panel>
     </v-expansion-panels>
+    <div v-else>
+      <v-alert type="error" :class="$style.alert">
+        合宿が選択されていません。管理者にお問い合わせください。
+      </v-alert>
+    </div>
   </div>
   <div></div>
 </template>
 
-<style>
+<style module>
 .container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100%;
+  height: 100vh;
 }
+
 .panel {
+  margin-top: 40px;
   max-width: 600px;
   width: 80%;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.title {
+  font-weight: bold;
+}
+
+.alert {
+  margin-top: 40px;
 }
 </style>
