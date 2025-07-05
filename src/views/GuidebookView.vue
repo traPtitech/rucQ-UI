dc6e89d5dc0381186957835255de3deb8d37c808
kitsne241 <bicycle.y.t@gmail.com>
2025-07-05 14:06:05 +0900

    合宿のしおりページを使用可能に

--- src/views/GuidebookView.vue
+++ src/views/GuidebookView.vue
@@ -1,17 +1,33 @@
 <template>
-  <MarkdownPreview :isEditable="false" v-model:text="text" />
+  <div :class="$style.container">
+    <div :class="$style.content">
+      <markdown-preview :isEditable="false" v-model:text="guidebookContent" />
+      <!-- ここに見出し一覧、クリックで飛べるようにする -->
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { apiClient } from '@/api/apiClient'
+import { computed } from 'vue'
 import MarkdownPreview from '@/components/markdown/MarkdownPreview.vue'
+import { useCampStore } from '@/store'
+import { storeToRefs } from 'pinia'
 
-const text = ref('')
+const { displayCamp } = storeToRefs(useCampStore())
 
-onMounted(async () => {
-  text.value = (await apiClient.GET('/api/camps/default')).data!.description
-})
+const guidebookContent = computed(() => displayCamp.value?.guidebook || '')
 </script>
 
-<style module></style>
+<style module>
+.container {
+  width: 100%;
+  height: 100%;
+  padding: 40px 16px;
+  display: flex;
+}
+
+.content {
+  flex-grow: 1;
+  height: 100%;
+}
+</style>
