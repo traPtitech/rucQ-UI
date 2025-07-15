30feabbda687845e61ef09beb04c8a1f57f15052
kitsne241 <bicycle.y.t@gmail.com>
2025-07-15 11:51:22 +0900

    add: ESLint に関する発展的な設定

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,35 @@
+import { defineConfig } from 'eslint-define-config'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default [
+export default defineConfig([
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    ignores: ['dist', 'node_modules', '.output', 'coverage'],
   },
 
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+
   {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        project: ['./tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+        extraFileExtensions: ['.vue'],
+      },
+    },
   },
 
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  skipFormatting,
-]
+  eslintConfigPrettier,
+
+  {
+    rules: {
+      'vue/component-name-in-template-casing': ['warn', 'kebab-case'], // kebab-case 推奨
+    },
+  },
+])
