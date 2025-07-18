705fb74a2e15b53bd847966821777bf2c6186740
mumumu <takumu.sato@gmail.com>
2025-07-18 18:02:41 +0900

    Revert "format"
    This reverts commit 9bc208959f0646d7c5fe3252e698d5200868b126.

--- src/components/layout/PageNavigation.vue
+++ src/components/layout/PageNavigation.vue
@@ -46,14 +46,7 @@ const navItems = [
 </script>
 
 <template>
-  <v-bottom-navigation
-    v-if="xs"
-    color="primary"
-    v-model="currentPath"
-    mandatory
-    grow
-    :class="$style.bottomNavigation"
-  >
+  <v-bottom-navigation v-if="xs" color="primary" v-model="currentPath" mandatory grow :class="$style.bottomNavigation">
     <v-btn
       v-for="item in navItems"
       :value="fullPath(item.path)"
