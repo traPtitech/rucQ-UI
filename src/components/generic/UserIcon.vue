296888be7f22550af0d5e9c4dca8743279cca94f
kitsne <bicycle.y.t@gmail.com>
2025-08-10 18:02:50 +0900

    feat: ユーザーアイコンをクリックでIDを表示可能に

--- src/components/generic/UserIcon.vue
+++ src/components/generic/UserIcon.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { useUserStore } from '@/store'
 import { storeToRefs } from 'pinia'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const { user } = storeToRefs(useUserStore())
-const props = defineProps<{ id?: string; size?: number }>()
+const props = defineProps<{ id?: string; size?: number; idTooltip?: boolean }>()
+// idTooltip ... クリック時に Tooltip で ID を表示するかどうか
+
+const showTooltip = ref(false)
 
 const imageStyle = computed(
   () =>
@@ -14,10 +17,37 @@ const imageStyle = computed(
       objectFit: 'contain',
       borderRadius: `${props.size || 0}px`,
       display: 'block',
+      cursor: props.idTooltip ? 'pointer' : 'default',
     }) as const,
 )
+
+const userId = computed(() => props.id || user.value?.id)
+const tooltipText = computed(() => `@${userId.value}`)
+
+const handleClick = () => {
+  if (props.idTooltip) {
+    showTooltip.value = true
+  }
+}
+
+// tooltipProps からマウスイベントを除外
+const getModifiedTooltipProps = (tooltipProps: Record<string, unknown>) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { onMouseenter, ...rest } = tooltipProps
+  return rest
+}
 </script>
 
 <template>
-  <img :style="imageStyle" :src="`https://q.trap.jp/api/v3/public/icon/${id || user?.id}`" />
+  <v-tooltip v-if="idTooltip" v-model="showTooltip" :text="tooltipText" location="top">
+    <template #activator="{ props: tooltipProps }">
+      <img
+        :style="imageStyle"
+        :src="`https://q.trap.jp/api/v3/public/icon/${userId}`"
+        v-bind="getModifiedTooltipProps(tooltipProps)"
+        @click="handleClick"
+      />
+    </template>
+  </v-tooltip>
+  <img v-else :style="imageStyle" :src="`https://q.trap.jp/api/v3/public/icon/${userId}`" />
 </template>
