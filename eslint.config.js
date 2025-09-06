782c3bf13a28c6d46b6afc8fc898f8c4826a6bee
kitsne <bicycle.y.t@gmail.com>
2025-09-07 00:46:57 +0900

    feat: ESLint のセキュリティ周りの設定を更新

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'eslint-define-config'
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
+import pluginSecurity from 'eslint-plugin-security'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
@@ -12,6 +13,7 @@ export default defineConfig([
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
+  pluginSecurity.configs.recommended,
 
   {
     files: ['**/*.vue'],
@@ -30,6 +32,8 @@ export default defineConfig([
   {
     rules: {
       'vue/component-name-in-template-casing': ['warn', 'kebab-case'], // kebab-case 推奨
+      'vue/no-template-target-blank': ['error', { enforceDynamicLinks: 'always' }],
+      'vue/no-v-html': 'error',
     },
   },
 ])
