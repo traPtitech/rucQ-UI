9340276ee192027abd090d5cb8412d90ccb66800
kanimiso <terazawa.y.270c@m.isct.ac.jp>
2026-01-23 19:05:36 +0900

    feat: ヘッダーにキャンプの状態に応じたアイコンを表示する機能を追加

--- src/components/layout/HeaderButton.vue
+++ src/components/layout/HeaderButton.vue
@@ -2,18 +2,47 @@
 import { useRoute } from 'vue-router'
 import { useDisplay } from 'vuetify'
 import HeaderButtonList from './HeaderButtonList.vue'
-
+import { useTimeStore } from '@/store'
+import { computed } from 'vue'
+import { useCampStore } from '@/store'
 const route = useRoute()
 const { xs } = useDisplay()
+const timeStore = useTimeStore()
+const campStore = useCampStore()
+
+const displayCamp = computed(() => {
+  const campname = route.params.campname as string | undefined
+  if (!campname) return null
+  try {
+    return campStore.getCampByDisplayId(campname)
+  } catch {
+    return null
+  }
+})
+
+const isArchived = computed(() => {
+  return displayCamp.value ? timeStore.isCampEnded(displayCamp.value) : false
+})
+
+const campName = computed(() => displayCamp.value?.name)
+
+const campIconUrl = computed(() => {
+  if (isArchived.value) {
+    return '/icons/icon-transparent-archived.svg'
+  } else {
+    return '/icons/icon-transparent.svg'
+  }
+})
+
 </script>
 
 <template>
   <v-app-bar v-if="xs" elevation="2" color="white" density="comfortable" :class="$style.appBar">
     <template #prepend>
-      <img src="/icons/icon-transparent.svg" alt="rucQ Icon" :class="$style.icon" />
+      <img :src="campIconUrl" alt="rucQ Icon" :class="$style.icon" />
     </template>
     <v-app-bar-title class="text-primary">
-      <span :class="$style.routeTitle">{{ route.name }}</span>
+      <span :class="$style.routeTitle">{{ route.name }} </span>
     </v-app-bar-title>
     <v-menu>
       <template #activator="{ props: activatorProps }">
