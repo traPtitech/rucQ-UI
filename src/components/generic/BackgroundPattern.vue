06d19ad1d75e691c03256645f3fb8c6255a399b3
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2026-01-26 14:52:11 +0900

    背景の表示方法を変更

--- src/components/generic/BackgroundPattern.vue
+++ src/components/generic/BackgroundPattern.vue
@@ -29,10 +29,10 @@ const bgColor = computed(() => {
 <style module>
 .background {
   position: fixed !important;
-  top: 0vh;
-  left: 0vw;
-  height: 100dvh;
-  width: 100vw;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   display: flex;
   justify-content: center;
   align-items: center;
