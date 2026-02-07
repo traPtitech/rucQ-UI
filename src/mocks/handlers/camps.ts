4857a83d1d2619c91abd64f102fb43852d2f997f
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2026-02-07 22:35:54 +0900

    camps のモックデータをもとに戻す

--- src/mocks/handlers/camps.ts
+++ src/mocks/handlers/camps.ts
@@ -99,24 +99,6 @@ def fibonacci(n):
 # 使用例
 for i in range(10):
     print(f"F({i}) = {fibonacci(i)}")
-
-def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n-1) + fibonacci(n-2)
-
-# 使用例
-for i in range(10):
-    print(f"F({i}) = {fibonacci(i)}")
-
-def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n-1) + fibonacci(n-2)
-
-# 使用例
-for i in range(10):
-    print(f"F({i}) = {fibonacci(i)}")
 \`\`\`
 
 ### チーム開発ワークショップ
