4ce41174b8f82dbebad127e3486cd385d4be600b
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2026-02-15 12:09:42 +0900

    gap-1 -> ga-1

--- src/components/log/RollCallLog.vue
+++ src/components/log/RollCallLog.vue
@@ -22,18 +22,18 @@ defineProps<{
     :active="log.status !== 'excluded'"
   >
     <template #default="{ color }">
-      <div class="w-100 h-100 d-flex justify-space-between align-center">
+      <div class="w-100 h-100 d-flex justify-space-between ga-2 align-center">
         <div>
           <div :class="`text-${color}`">{{ log.title }}</div>
-          <div v-if="log.status === 'answered'" class="d-flex align-center gap-1">
+          <div v-if="log.status === 'answered'" class="d-flex align-center ga-1">
             <v-icon size="16" icon="mdi-check" color="green" />
             <span class="ml-1 text-caption" :class="$style.caption">回答済み</span>
           </div>
-          <div v-else-if="log.status === 'unanswered'" class="d-flex align-center gap-1">
+          <div v-else-if="log.status === 'unanswered'" class="d-flex align-center ga-1">
             <v-icon size="16" icon="mdi-alert-circle-outline" color="red" />
             <span class="ml-1 text-caption" :class="$style.caption">未回答</span>
           </div>
-          <div v-else class="d-flex align-center gap-1">
+          <div v-else class="d-flex align-center ga-1">
             <v-icon size="16" icon="mdi-minus-circle-outline" color="grey" />
             <span class="ml-1 text-caption" :class="$style.caption">対象外</span>
           </div>
